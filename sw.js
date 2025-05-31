325f90426f6878f296db3688feba06bfab72fc00
Bolzy99 <yash.super50@gmail.com>
2025-05-31 20:11:42 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'gym-pwa-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install event - cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activate event - clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event - serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
